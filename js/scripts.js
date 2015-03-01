3ed669b0647f985b73419aa7e2e78a995465c14f
Alex Devero <deveroalex@gmail.com>
2015-03-01 12:59:08 +0100

    Update scripts.js
    Added code for resizing and moving the main element when social media sidebar slides from left.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(function() {
   $(".js-aside-toggle").click(function(e) {
-    console.log(e);
     var socialMediaNav = $(".js-aside");
     if (socialMediaNav.hasClass("hidden")) {
       socialMediaNav.removeClass("hidden");
       socialMediaNav.animate({
         "left": 0
+      });
+      $("main").animate({
+        "margin-left": "100px",
+        "width": $("main").width() - 100
       });
     } else {
       socialMediaNav.animate({
@@ -13,6 +16,10 @@ $(function() {
       }).delay(200).queue(function() {
         $(this).addClass("hidden");
         $(this).dequeue();
+      });
+      $("main").animate({
+        "margin-left": 0,
+        "width": "100%"
       });
     }
   });
